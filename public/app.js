37d66c5ed101dc928380f6f2ddfbfb08b7f559ad
Steven Gallagher <stevegmag@gmail.com>
2025-01-15 13:22:06 -0500

    updated todo list

--- public/app.js
+++ public/app.js
@@ -2,13 +2,11 @@ import { Invoice } from './classes/Invoice.js';
 import { Payment } from './classes/Payment.js';
 import { ListTemplate } from './classes/ListTemplate.js';
 const form = document.querySelector('.new-item-form');
-// inputs
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
 const total = document.querySelector('#total');
-// list template instance
 const ul = document.querySelector('ul');
 const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
@@ -25,8 +23,4 @@ form.addEventListener('submit', (e) => {
         total.innerText = (parseInt(total.innerText) - amount.valueAsNumber).toString();
     }
     list.render(doc, type.value, 'end');
-    /* TODO:
-    - [X] Add a delete button to each item in the list
-    - [X] Add a total at the bottom of the list
-    */
 });
